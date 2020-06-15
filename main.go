0a3bd1a14340b5fee5d89b6ec360d945f1afad4e
Kamshory, MT <kamshory@yahoo.com>
2020-06-15 09:47:42 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,21 +13,29 @@ import (
 	"regexp"
 	"strings"
 	"time"
-
-
 	"github.com/auth0/go-jwt-middleware"
 	"github.com/dgrijalva/jwt-go"
-
-	b64 "encoding/base64"
+	"encoding/base64"
 	"encoding/hex"
 	"encoding/json"
 )
 
 const (
-	TokenKey = "RAHASIA"
-	API_KEY = "dev_akey_U1AQQ7Z1PP"
-	VALIDATION_KEY = "dev_vkey_VPT6U7M3HC"
-	DEFAULT_URL_INQUIRY = "http://localhost:8888/bank/va/inquiry"
+	constBankCode              = "200"
+	constTokenKey              = "RAHASIA"
+	constAPIKey                = "dev_akey_U1AQQ7Z1PP"
+	constValidationKey         = "dev_vkey_VPT6U7M3HC"
+	constDefaultURLInquiry     = "http://localhost:8081/process-va"
+	constDefaultURLPayment     = "http://localhost:8081/process-va"
+	constDefaultURLCheckStatus = "http://localhost:8081/process-va"
+
+	responseCodeSuccess        = "00"
+	responseCodeFailed         = "01"
+
+	responseTextSuccess        = "Success"
+	responseTextFailed         = "Failed"
+
+	defaultCurrencyCode        = "IDR"
 )
 
 func main() {
@@ -36,22 +44,22 @@ func main() {
 	/**
 	 * Token handler
 	 */
-	http.HandleFunc("/bank/auth/token/", TokenHandler)
+	http.HandleFunc("/bank/auth/token/", tokenHandler)
 	
 	/**
 	 * Inquiry handler
 	 */
-	 http.Handle("/bank/va/inquiry/", AuthMiddleware(http.HandlerFunc(InquiryHandler)))
+	 http.Handle("/bank/va/inquiry/", authMiddleware(http.HandlerFunc(inquiryHandler)))
 	
 	/**
 	 * Payment handler
 	 */
-	http.Handle("/bank/va/payment", AuthMiddleware(http.HandlerFunc(PaymentHandler)))
+	http.Handle("/bank/va/payment/", authMiddleware(http.HandlerFunc(paymentHandler)))
 	
 	/**
 	 * Check status handler
 	 */
-    http.Handle("/bank/va/check-status", AuthMiddleware(http.HandlerFunc(CheckStatusHandler)))
+    http.Handle("/bank/va/check-status/", authMiddleware(http.HandlerFunc(checkStatusHandler)))
 
     if err := http.ListenAndServe(":8013", nil); err != nil {
         log.Fatal(err)
@@ -61,8 +69,7 @@ func main() {
 
 // TokenHandler is our handler to take a username and password and,
 // if it's valid, return a token used for future requests.
-func TokenHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println(r.Method)
+func tokenHandler(w http.ResponseWriter, r *http.Request) {
     w.Header().Add("Content-Type", "application/json")
     r.ParseForm()
 
@@ -72,7 +79,7 @@ func TokenHandler(w http.ResponseWriter, r *http.Request) {
 	auth := r.Header.Get("Authorization");
 	userpassEnc := strings.SplitN(auth, " ", 2)
 	base64String := userpassEnc[1]
-	sDec,_ := b64.StdEncoding.DecodeString(base64String)
+	sDec,_ := base64.StdEncoding.DecodeString(base64String)
 	userpass := strings.SplitN(string(sDec), ":", 2)
 
 	username := userpass[0]
@@ -91,7 +98,7 @@ func TokenHandler(w http.ResponseWriter, r *http.Request) {
         "exp":  expire,
         "iat":  time.Now().Unix(),
     })
-    tokenString, err := token.SignedString([]byte(TokenKey))
+    tokenString, err := token.SignedString([]byte(constTokenKey))
     if err != nil {
         w.WriteHeader(http.StatusInternalServerError)
         io.WriteString(w, `{"error":"token_generation_failed"}`)
@@ -103,44 +110,45 @@ func TokenHandler(w http.ResponseWriter, r *http.Request) {
 
 // AuthMiddleware is our middleware to check our token is valid. Returning
 // a 401 status to the client if it is not valid.
-func AuthMiddleware(next http.Handler) http.Handler {
-    if len(TokenKey) == 0 {
-        log.Fatal("HTTP server unable to start, expected an TokenKey for JWT auth")
+func authMiddleware(next http.Handler) http.Handler {
+    if len(constTokenKey) == 0 {
+        log.Fatal("HTTP server unable to start, expected an constTokenKey for JWT auth")
     }
     jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
         ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-            return []byte(TokenKey), nil
+            return []byte(constTokenKey), nil
         },
         SigningMethod: jwt.SigningMethodHS256,
     })
     return jwtMiddleware.Handler(next)
 }
-func CreateSignature(httpMethod string, path string, token string, timestamp string, body string) string{
-	validationKey := os.Getenv("VALIDATION_KEY")
+func createSignature(httpMethod string, path string, token string, timeStamp string, body string) string{
+	validationKey := os.Getenv("VA_VALIDATION_KEY")
 	if len(validationKey) == 0{
-		validationKey = VALIDATION_KEY
+		validationKey = constValidationKey
 	}
 	regexSpace := regexp.MustCompile(`\s+`)
 	canonicalBody := regexSpace.ReplaceAllString(body, "")
 	h := sha256.New()
 	h.Write([]byte(canonicalBody))
-	sha := h.Sum(nil)  // "sha" is uint8 type, encoded in base16
+	sha := h.Sum(nil) 
 	sha256Body := hex.EncodeToString(sha)
-	stringToSign := fmt.Sprintf("%s:%s:%s:%s:%s", httpMethod, path, token, sha256Body, timestamp)
-	log.Print(body)
-	log.Print(canonicalBody)
-	log.Print(sha256Body)
-	log.Print(stringToSign)
+	stringToSign := fmt.Sprintf("%s:%s:%s:%s:%s", httpMethod, path, token, sha256Body, timeStamp)
 	hash := hmac.New(sha256.New, []byte(validationKey))
 	io.WriteString(hash, stringToSign)
 	signtaure := fmt.Sprintf("%x", hash.Sum(nil))
 	return signtaure
 }
-func InquiryHandler(w http.ResponseWriter, r *http.Request) {
-
+func inquiryHandler(w http.ResponseWriter, r *http.Request) {
+	timeStamp := time.Now().UTC().Format(time.RFC3339Nano)
+	timeStamp = string(timeStamp[0:23]) + "Z"
+	bankCode := os.Getenv("VA_BANK_CODE")
+	if len(bankCode) == 0{
+		bankCode = constBankCode
+	}
 	urlInquiry := os.Getenv("VA_URL_INQUIRY")
 	if len(urlInquiry) == 0{
-		urlInquiry = DEFAULT_URL_INQUIRY
+		urlInquiry = constDefaultURLInquiry
 	}
 	token := r.Header.Get("Authorization")
 	if len(token) > 7{
@@ -152,48 +160,339 @@ func InquiryHandler(w http.ResponseWriter, r *http.Request) {
     if err != nil {
         log.Fatal(err)
     }
-	fmt.Printf("%s", reqBody)
-	
-	fmt.Print(reqBody)
-
 	if err != nil {
 		log.Printf("Error reading body: %v", err)
 		http.Error(w, "can't read body", http.StatusBadRequest)
 		return
 	}
-	signature1 := CreateSignature(r.Method, r.RequestURI, token, r.Header.Get("x-bank-timestamp"), string(reqBody))
+	signature1 := createSignature(r.Method, r.RequestURI, token, r.Header.Get("x-bank-timestamp"), string(reqBody))
 	signature2 := r.Header.Get("x-bank-signature")
-	fmt.Println("Signature Generated : "+signature1)
-	fmt.Println("Signature Sent      : "+signature2)
-	if(signature1 == signature2){
+	if(signature1 != signature2){
 		log.Printf("Invalid signature")
-		http.Error(w, "can't read body", http.StatusBadRequest)
+		http.Error(w, "Can't read body", http.StatusBadRequest)
 		return
 	}
 
-	jsonRequest := make(map[string]interface{})
-	json.Unmarshal(reqBody, &jsonRequest)
+	var jsonRequest map[string]interface{}
+	json.Unmarshal([]byte(reqBody), &jsonRequest)
 
-	buf, _ := json.Marshal(jsonRequest)
+	// Add bank code
+	jsonRequest["data"].(map[string]interface{})["bank_code"] = bankCode
+
+	bufRequest, _ := json.Marshal(jsonRequest)
 	
-	res, err := http.Post(urlInquiry, "application/json", bytes.NewBuffer(buf))
-	data, _ := ioutil.ReadAll(res.Body)
-	res.Body.Close()
+	response, err := http.Post(urlInquiry, "application/json", bytes.NewBuffer(bufRequest))
+	
+	var responseBody string
+	var responseCode string
+	var responseText string
 
-	if res.Request.Response.Status == "200"{
+	if err != nil{
+		log.Println("Terjadi error di sini")
 
+		var jsonResponse map[string]interface{}
+		json.Unmarshal([]byte(reqBody), &jsonResponse)
+
+		delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+		jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+		jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+		jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""
+
+		jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+
+		// Delete pg_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		jsonResponse["response_code"] = responseCodeFailed
+		jsonResponse["response_text"] = responseTextFailed
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
+	}else{
+		responseFromService, empty := ioutil.ReadAll(response.Body)
+		var jsonResponse map[string]interface{}
+		if empty != nil{
+			json.Unmarshal([]byte(reqBody), &jsonResponse)
+		}else{
+			json.Unmarshal([]byte(responseFromService), &jsonResponse)
+
+			if str, ok:= jsonResponse["response_code"].(string); ok{
+				responseCode = str;
+			}else{
+				fmt.Println("Ga dapat response code di sini")
+				json.Unmarshal([]byte(reqBody), &jsonResponse)
+				delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+				jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+				jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+				jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""		
+				jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+				responseCode = responseCodeFailed
+			}
+			if str, ok:= jsonResponse["response_text"].(string); ok{
+				responseText = str;
+			}else{
+				responseText = responseTextFailed
+			}
+		}
+
+		jsonResponse["response_code"] = responseCode
+		jsonResponse["response_text"] = responseText
+
+		// Delete pg_code and bank_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		delete(jsonResponse["data"].(map[string]interface{}), "bank_code")
+		
+		// Delete log_data from client
+		delete(jsonResponse, "log_data")
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
 	}
-	fmt.Println(string(data))
-
-    io.WriteString(w, `{"status":"ok", "command":"inquiry"}`)
+	io.WriteString(w, responseBody)
+	
 }
 
-func PaymentHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "application/json")
-    io.WriteString(w, `{"status":"ok", "command":"payment"}`)
+func paymentHandler(w http.ResponseWriter, r *http.Request) {
+    timeStamp := time.Now().UTC().Format(time.RFC3339Nano)
+	timeStamp = string(timeStamp[0:23]) + "Z"
+	bankCode := os.Getenv("VA_BANK_CODE")
+	if len(bankCode) == 0{
+		bankCode = constBankCode
+	}
+	urlInquiry := os.Getenv("VA_URL_PAYMENT")
+	if len(urlInquiry) == 0{
+		urlInquiry = constDefaultURLPayment
+	}
+	token := r.Header.Get("Authorization")
+	if len(token) > 7{
+		token = string(token[7:])
+	}
+	w.Header().Add("Content-Type", "application/json")
+	
+	reqBody, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+	signature1 := createSignature(r.Method, r.RequestURI, token, r.Header.Get("x-bank-timestamp"), string(reqBody))
+	signature2 := r.Header.Get("x-bank-signature")
+	if(signature1 != signature2){
+		log.Printf("Invalid signature")
+		http.Error(w, "Can't read body", http.StatusBadRequest)
+		return
+	}
+
+	var jsonRequest map[string]interface{}
+	json.Unmarshal([]byte(reqBody), &jsonRequest)
+
+	// Add bank code
+	jsonRequest["data"].(map[string]interface{})["bank_code"] = bankCode
+
+	bufRequest, _ := json.Marshal(jsonRequest)
+	
+	response, err := http.Post(urlInquiry, "application/json", bytes.NewBuffer(bufRequest))
+	
+	var responseBody string
+	var responseCode string
+	var responseText string
+
+	if err != nil{
+		log.Println("Terjadi error di sini")
+
+		var jsonResponse map[string]interface{}
+		json.Unmarshal([]byte(reqBody), &jsonResponse)
+
+		delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+		jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+		jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+		jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""
+
+		jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+
+		// Delete pg_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		jsonResponse["response_code"] = responseCodeSuccess
+		jsonResponse["response_text"] = responseTextSuccess
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
+	}else{
+		responseFromService, empty := ioutil.ReadAll(response.Body)
+		var jsonResponse map[string]interface{}
+		if empty != nil{
+			json.Unmarshal([]byte(reqBody), &jsonResponse)
+		}else{
+			json.Unmarshal([]byte(responseFromService), &jsonResponse)
+
+			if str, ok:= jsonResponse["response_code"].(string); ok{
+				responseCode = str;
+			}else{
+				fmt.Println("Ga dapat response code di sini")
+				json.Unmarshal([]byte(reqBody), &jsonResponse)
+				delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+				jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+				jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+				jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""		
+				jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+				responseCode = responseCodeSuccess
+			}
+			if str, ok:= jsonResponse["response_text"].(string); ok{
+				responseText = str;
+			}else{
+				responseText = responseTextSuccess
+			}
+		}
+
+		jsonResponse["response_code"] = responseCode
+		jsonResponse["response_text"] = responseText
+
+		// Delete pg_code and bank_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		delete(jsonResponse["data"].(map[string]interface{}), "bank_code")
+		
+		// Delete log_data from client
+		delete(jsonResponse, "log_data")
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
+	}
+	io.WriteString(w, responseBody)
 }
 
-func CheckStatusHandler(w http.ResponseWriter, r *http.Request) {
-    w.Header().Add("Content-Type", "application/json")
-    io.WriteString(w, `{"status":"ok", "command":"check-status"}`)
+func checkStatusHandler(w http.ResponseWriter, r *http.Request) {
+    timeStamp := time.Now().UTC().Format(time.RFC3339Nano)
+	timeStamp = string(timeStamp[0:23]) + "Z"
+	bankCode := os.Getenv("VA_BANK_CODE")
+	if len(bankCode) == 0{
+		bankCode = constBankCode
+	}
+	urlInquiry := os.Getenv("VA_URL_CHECK_STATUS")
+	if len(urlInquiry) == 0{
+		urlInquiry = constDefaultURLCheckStatus
+	}
+	token := r.Header.Get("Authorization")
+	if len(token) > 7{
+		token = string(token[7:])
+	}
+	w.Header().Add("Content-Type", "application/json")
+	
+    reqBody, err := ioutil.ReadAll(r.Body)
+    if err != nil {
+        log.Fatal(err)
+    }
+	if err != nil {
+		log.Printf("Error reading body: %v", err)
+		http.Error(w, "can't read body", http.StatusBadRequest)
+		return
+	}
+	signature1 := createSignature(r.Method, r.RequestURI, token, r.Header.Get("x-bank-timestamp"), string(reqBody))
+	signature2 := r.Header.Get("x-bank-signature")
+	if(signature1 != signature2){
+		log.Printf("Invalid signature")
+		http.Error(w, "Can't read body", http.StatusBadRequest)
+		return
+	}
+
+	var jsonRequest map[string]interface{}
+	json.Unmarshal([]byte(reqBody), &jsonRequest)
+
+	// Add bank code
+	jsonRequest["data"].(map[string]interface{})["bank_code"] = bankCode
+
+	bufRequest, _ := json.Marshal(jsonRequest)
+	
+	response, err := http.Post(urlInquiry, "application/json", bytes.NewBuffer(bufRequest))
+	
+	var responseBody string
+	var responseCode string
+	var responseText string
+
+	if err != nil{
+		log.Println("Terjadi error di sini")
+
+		var jsonResponse map[string]interface{}
+		json.Unmarshal([]byte(reqBody), &jsonResponse)
+
+		delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+		jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+		jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+		jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+		jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+		jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""
+
+		jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+
+		// Delete pg_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		jsonResponse["response_code"] = responseCodeSuccess
+		jsonResponse["response_text"] = responseTextSuccess
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
+	}else{
+		responseFromService, empty := ioutil.ReadAll(response.Body)
+		var jsonResponse map[string]interface{}
+		if empty != nil{
+			json.Unmarshal([]byte(reqBody), &jsonResponse)
+		}else{
+			json.Unmarshal([]byte(responseFromService), &jsonResponse)
+
+			if str, ok:= jsonResponse["response_code"].(string); ok{
+				responseCode = str;
+			}else{
+				fmt.Println("Ga dapat response code di sini")
+				json.Unmarshal([]byte(reqBody), &jsonResponse)
+				delete(jsonResponse["data"].(map[string]interface{}), "date_time")
+				jsonResponse["data"].(map[string]interface{})["time_stamp"] = timeStamp
+				jsonResponse["data"].(map[string]interface{})["currency_code"] = defaultCurrencyCode
+				jsonResponse["data"].(map[string]interface{})["customer_name"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_email"] = ""
+				jsonResponse["data"].(map[string]interface{})["customer_phone"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_code"] = ""
+				jsonResponse["data"].(map[string]interface{})["merchant_name"] = ""		
+				jsonResponse["data"].(map[string]interface{})["bill_list"] = make([]string, 0)
+				responseCode = responseCodeSuccess
+			}
+			if str, ok:= jsonResponse["response_text"].(string); ok{
+				responseText = str;
+			}else{
+				responseText = responseTextSuccess
+			}
+		}
+
+		jsonResponse["response_code"] = responseCode
+		jsonResponse["response_text"] = responseText
+
+		// Delete pg_code and bank_code from client
+		delete(jsonResponse["data"].(map[string]interface{}), "pg_code")
+		delete(jsonResponse["data"].(map[string]interface{}), "bank_code")
+		
+		// Delete log_data from client
+		delete(jsonResponse, "log_data")
+
+		bufResponse, _ := json.Marshal(jsonResponse)
+		responseBody = string(bufResponse)
+	}
+	io.WriteString(w, responseBody)
 }
